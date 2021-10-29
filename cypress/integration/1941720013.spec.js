9ff06ac0b341020c8bc899962456d3bb30e5814e
Mochammad Syaifuddin Zuhri <1941720013@student.polinema.ac.id>
2021-10-29 23:35:41 +0700

    TC_DETAIL - Add test scenario 1 [Mochammad Syaifuddin Zuhri]

--- cypress/integration/1941720013.spec.js
+++ cypress/integration/1941720013.spec.js
@@ -12,5 +12,16 @@ describe('TC_DETAIL - 1941720013 - Mochammad Syaifuddin Zuhri', () => {
         cy.get('#login-button').click()
     })
 
+    // Test Scenario 1
+    it('1. Handle click the container product box', () => {
+
+        // Get First ProductBox to the example
+        const productBox = cy.get('.inventory_item').first()
+        productBox.click();
+
+        // Assertion then product box click is redirect to detail product page
+        cy.contains('[class="btn btn_secondary back btn_large inventory_details_back_button"]', 'Back to products').should('be.visible')
+    })
+
 
 })
