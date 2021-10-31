081b56d7a117d25026685b196d451418a8fab7c6
Mochammad Syaifuddin Zuhri <1941720013@student.polinema.ac.id>
2021-10-31 17:54:42 +0700

    Update TC_DETAIL

--- cypress/integration/1941720013.spec.js
+++ cypress/integration/1941720013.spec.js
@@ -12,56 +12,56 @@ describe('TC_DETAIL - 1941720013 - Mochammad Syaifuddin Zuhri', () => {
         cy.get('#login-button').click()
     })
 
-    // // Test Scenario 1
-    // it('1. Handle click the container product box', () => {
+    // Test Scenario 1
+    it('1. Handle click the container product box', () => {
 
-    //     // Get First ProductBox to the example
-    //     const productBox = cy.get('.inventory_item').first()
-    //     productBox.click();
+        // Get First ProductBox to the example
+        const productBox = cy.get('.inventory_item').first()
+        productBox.click();
 
-    //     // Assertion then product box click is redirect to detail product page is false
-    //     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
-    // })
+        // Assertion then product box click is redirect to detail product page is false
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+    })
 
-    // // Test Scenario 2
-    // it('2. Handle click the photo product box', () => {
+    // Test Scenario 2
+    it('2. Handle click the photo product box', () => {
 
-    //     // Get First Photo Product Box to the example
-    //     const photoProductBox = cy.get('.inventory_item_img').first()
-    //     photoProductBox.click();
+        // Get First Photo Product Box to the example
+        const photoProductBox = cy.get('.inventory_item_img').first()
+        photoProductBox.click();
 
-    //     // Assertion then product box click is redirect to detail product page
-    //     cy.contains('[class="btn btn_secondary back btn_large inventory_details_back_button"]', 'Back to products').should('be.visible')
-    // })
+        // Assertion then product box click is redirect to detail product page
+        cy.contains('[class="btn btn_secondary back btn_large inventory_details_back_button"]', 'Back to products').should('be.visible')
+    })
 
-    // // Test Scenario 3
-    // it('3. Display product detail data', () => {
+    // Test Scenario 3
+    it('3. Display product detail data', () => {
 
-    //     // Get First Photo Product Box to the example
-    //     const photoProductBox = cy.get('.inventory_item_img').first()
-    //     photoProductBox.click();
+        // Get First Photo Product Box to the example
+        const photoProductBox = cy.get('.inventory_item_img').first()
+        photoProductBox.click();
 
-    //     // Assertion detail product data
-    //     cy.contains('[class="inventory_details_name large_size"]', 'Sauce Labs Backpack').should('be.visible')
-    //     cy.contains('[class="inventory_details_desc large_size"]', 'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.').should('be.visible')
-    //     cy.contains('[class="inventory_details_price"]', '$29.99').should('be.visible')
-    //     cy.contains('[class="btn btn_primary btn_small btn_inventory"]', 'Add to cart').should('be.visible')
-    //     cy.get('[class="inventory_details_img_container"]').find('.inventory_details_img').should('be.visible')
-    // })
+        // Assertion detail product data
+        cy.contains('[class="inventory_details_name large_size"]', 'Sauce Labs Backpack').should('be.visible')
+        cy.contains('[class="inventory_details_desc large_size"]', 'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.').should('be.visible')
+        cy.contains('[class="inventory_details_price"]', '$29.99').should('be.visible')
+        cy.contains('[class="btn btn_primary btn_small btn_inventory"]', 'Add to cart').should('be.visible')
+        cy.get('[class="inventory_details_img_container"]').find('.inventory_details_img').should('be.visible')
+    })
 
-    // // Test Scenario 4
-    // it('4. Handle click back button from product detail page to list product', () => {
+    // Test Scenario 4
+    it('4. Handle click back button from product detail page to list product', () => {
 
-    //     // Get First Photo Product Box to the example
-    //     const photoProductBox = cy.get('.inventory_item_img').first()
-    //     photoProductBox.click();
+        // Get First Photo Product Box to the example
+        const photoProductBox = cy.get('.inventory_item_img').first()
+        photoProductBox.click();
 
-    //     // Back button action
-    //     cy.get('[class="btn btn_secondary back btn_large inventory_details_back_button"]').click();
+        // Back button action
+        cy.get('[class="btn btn_secondary back btn_large inventory_details_back_button"]').click();
 
-    //     // Assertion then product box click is redirect to detail product page
-    //     cy.get('[class="header_secondary_container"]').find('.title').should('be.visible')
-    // })
+        // Assertion then product box click is redirect to detail product page
+        cy.get('[class="header_secondary_container"]').find('.title').should('be.visible')
+    })
 
     // Test Scenario 5
     it('5. Display each product data on the product list page', () => {
